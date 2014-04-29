70923c223755fb264b2e14d43b2bb14929ea5433
walkskyer <walkskyer@qq.com>
2014-04-29 11:28:39 +0800

    hello world

--- DjangoTest/urls.py
+++ DjangoTest/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
+from mysite.views import hello
 
 urlpatterns = patterns('',
     # Examples:
@@ -9,4 +10,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    ('^hello/$', hello),
 )
