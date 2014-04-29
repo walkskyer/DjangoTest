c7ce952900dcd18acf1beed1572dae3a5c9fcc76
walkskyer <walkskyer@qq.com>
2014-04-29 14:49:33 +0800

    使用render_to_response渲染视图模板。

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 
 # Create your views here.
 from django.template.loader import get_template
@@ -24,5 +24,4 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404()
     dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    return render_to_response('hours_ahead.html', {'offset': offset, 'dt': dt})
