985e0d29f38ef4603a5885e43a571b24a2b05fa5
walkskyer <walkskyer@qq.com>
2014-04-29 14:41:38 +0800

    使用模板。

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.template.loader import get_template
+from django.template import Context
 from django.http import HttpResponse, Http404
 import datetime
 
@@ -11,7 +13,8 @@ def hello(request):
 
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
+    t = get_template('current_datetime.html')
+    html = t.render(Context({'time': now}))
     return HttpResponse(html)
 
 
